a741f61e1925fd6e0cae0f4266450189ff4ccf9f
RCBA_91 <rbsoft07@gmail.com>
2025-09-15 16:28:08 -0400

    Cambios para REST Anotacion GET y POST

--- src/main/java/org/websoft/ws/EmpleadoServicesWS.java
+++ src/main/java/org/websoft/ws/EmpleadoServicesWS.java
@@ -3,35 +3,35 @@
  */
 package org.websoft.ws;
 
-
-
 import java.util.List;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.GenericEntity;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.websoft.entity.Empleado;
 import org.websoft.services.EmpleadoServices;
 
 /**
- * @author rbsoft
- * WebServices creado usando la dependecia Jersey
+ * @author rbsoft WebServices creado usando la dependecia Jersey
  */
 @Path("/empleado")
 public class EmpleadoServicesWS {
-	
-	private EmpleadoServices empservices = new  EmpleadoServices();
+
+	private EmpleadoServices empservices = new EmpleadoServices();
 
 	@GET
 	@Path("/test")
 	public String test() {
 		return "Mostrado dato test del Web Services";
 	}
-	
+
 	@GET
 	@Path("consultaEmpleado")
 	@Produces(MediaType.APPLICATION_JSON)
@@ -39,7 +39,7 @@ public class EmpleadoServicesWS {
 	public Empleado consultaEmpleado() {
 		return empservices.consultarEmpleado();
 	}
-	
+
 	@GET
 	@Path("empleados")
 	@Produces(MediaType.APPLICATION_JSON)
@@ -47,13 +47,47 @@ public class EmpleadoServicesWS {
 	public List<Empleado> empleados() {
 		return empservices.lstEmpleados();
 	}
-	
+
 	@GET
 	@Path("empleados/{id}")
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
 	public Empleado empleadoId(@PathParam("id") String idEmp) {
 		return empservices.consultarEmpleadoId(idEmp);
+	}
+
+	@GET
+	@Path("empleadosPorId/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response empleadoById(@PathParam("id") String idEmp) {
+		Empleado objEmpleado = empservices.consultarEmpleadoId(idEmp);
+
+		if (objEmpleado == null) {
+			return Response.noContent().build();
+
+		}
+		
+		GenericEntity<Empleado> empleadoGeneric = new GenericEntity<Empleado>(objEmpleado, Empleado.class);
+		
+		return Response.ok(empleadoGeneric).build();
+	}
+
+	@POST
+	@Path("guardarEmpleado")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response guardarEmpleado(Empleado empleado) {
+		
+		if (empleado == null) {
+			return Response.status(400).entity("No se ingresó informacion del empleado, favor verificar los datos ").build();
+		}
+		if (empleado.getNombre() == null || empleado.getNombre().isEmpty()) {
+			return Response.status(400).entity("El nombre es Requerido").build();
+		}
+		
+		GenericEntity<Empleado> empleadoGenericResult = new GenericEntity<Empleado>(empleado, Empleado.class);
+		return Response.ok(empleadoGenericResult).build();
 	}
 	
 	
